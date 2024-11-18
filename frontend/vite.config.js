dd7f948a24583d645eb36c386a1477e4465163b7
chirag jaiswal <121719508+chiragjaiswal20082002@users.noreply.github.com>
2024-11-18 08:00:54 +0530

    14th attempt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 
 // Vite config
-=======
->>>>>>> 43b87040531a2cf8608be74426ef8468d9a90d47
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -15,19 +14,19 @@ export default defineConfig({
     },
   },
   build: {
-<<<<<<< HEAD
+
     chunkSizeWarningLimit: 1000, // Increase chunk size warning limit
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'redux', 'axios'],
-=======
+
     chunkSizeWarningLimit: 1000, // 1000 KB (1 MB)
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'redux', 'axios'] // Example, include large libraries in separate chunk
->>>>>>> 43b87040531a2cf8608be74426ef8468d9a90d47
+
         },
       },
     },
